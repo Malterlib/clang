9b4301957d3abba283c1b11a7e372e6dcf1c6316
Erik Olofsson <erik@olofsson.info>
2017-04-22 14:31:58 +0200

    Remove sized allocation default visibility

--- lib/Sema/SemaExprCXX.cpp
+++ lib/Sema/SemaExprCXX.cpp
@@ -2709,8 +2709,8 @@ void Sema::DeclareGlobalAllocationFunction(DeclarationName Name,
     Alloc->setVisibleDespiteOwningModule();
 
     // Implicit sized deallocation functions always have default visibility.
-    Alloc->addAttr(
-        VisibilityAttr::CreateImplicit(Context, VisibilityAttr::Default));
+//    Alloc->addAttr(
+//        VisibilityAttr::CreateImplicit(Context, VisibilityAttr::Default));
 
     llvm::SmallVector<ParmVarDecl *, 3> ParamDecls;
     for (QualType T : Params) {
